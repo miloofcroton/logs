c7d427f6f02bb575a9851b5fe699ca04f02b4e47
Jack Toumey <jack@toumey.io>
2025-10-03 12:22:17 -0700

    adding some error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+// use std::{fs, io::Error};
+use std::fs;
+use std::io::Error;
+
+fn divide(a: f64, b: f64) -> Result<f64, Error> {
+  if b == 0.0 {
+    Err(Error::other("can't divide by zero"))
+  }
+  else {
+    Ok(a / b)
+  }
+}
+
+fn validateEmail(email: String) -> Result<(), Error>{
+  if email.contains("@") {
+    Ok(())
+  }
+  else {
+    Err(Error::other("email is missing @ symbol"))
+  }
+}
+
 fn main() {
-  println!("Hello, world!");
+  // let text = fs::read_to_string("logs.txt");
+
+  // println!("{:#?}", text);
+
+  match divide(5.0, 0.0) {
+    Ok(result) => {
+      println!("{}", result)
+    }
+    Err(err) => {
+      println!("{}", err)
+    }
+  }
+
+  match validateEmail(String::from("test@example.com")) {
+    Ok(..) => println!("email is valid"),
+    Err(errMsg) => println!("{}", errMsg),
+  }
 }

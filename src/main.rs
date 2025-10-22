96f7fb32d4ba888e94b349c133ee16f16d2fe947
Jack Toumey <jack@toumey.io>
2025-10-22 14:45:21 -0700

    working on extract errors fn

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,62 @@
+use core::error;
 // use std::{fs, io::Error};
 use std::fs;
 use std::io::Error;
 
-fn divide(a: f64, b: f64) -> Result<f64, Error> {
-  if b == 0.0 {
-    Err(Error::other("can't divide by zero"))
-  }
-  else {
-    Ok(a / b)
-  }
-}
+// fn divide(a: f64, b: f64) -> Result<f64, Error> {
+//   if b == 0.0 {
+//     Err(Error::other("can't divide by zero"))
+//   }
+//   else {
+//     Ok(a / b)
+//   }
+// }
 
-fn validateEmail(email: String) -> Result<(), Error>{
-  if email.contains("@") {
-    Ok(())
+// fn validateEmail(email: String) -> Result<(), Error>{
+//   if email.contains("@") {
+//     Ok(())
+//   }
+//   else {
+//     Err(Error::other("email is missing @ symbol"))
+//   }
+// }
+
+fn extractErrors(text: &str) -> Vec<&str> {
+  let splitText = text.split("\n");
+
+  let mut results = vec![];
+
+  for line in splitText {
+    if line.starts_with("ERROR") {
+      results.push(line);
+    }
   }
-  else {
-    Err(Error::other("email is missing @ symbol"))
-  }
+
+  return results;
 }
 
 fn main() {
-  // let text = fs::read_to_string("logs.txt");
+  // match divide(5.0, 0.0) {
+  //   Ok(result) => {
+  //     println!("{}", result)
+  //   }
+  //   Err(err) => {
+  //     println!("{}", err)
+  //   }
+  // }
 
-  // println!("{:#?}", text);
+  // match validateEmail(String::from("test@example.com")) {
+  //   Ok(..) => println!("email is valid"),
+  //   Err(errMsg) => println!("{}", errMsg),
+  // }
 
-  match divide(5.0, 0.0) {
-    Ok(result) => {
-      println!("{}", result)
+  match fs::read_to_string("logs.txt") {
+    Ok(text) => {
+      let errorLogs = extractErrors(&text);
+      println!("{:#?}", errorLogs)
     }
-    Err(err) => {
-      println!("{}", err)
+    Err(errMsg) => {
+      println!("{}", errMsg)
     }
-  }
-
-  match validateEmail(String::from("test@example.com")) {
-    Ok(..) => println!("email is valid"),
-    Err(errMsg) => println!("{}", errMsg),
   }
 }
